f38cb7517d79af56528247705b7a84f82d359020
Ibrahim <ibs@pop-os.localdomain>
2024-06-29 19:56:18 +0100

    Set up docker container for the api

---
+++ app/api.py
@@ -0,0 +1,167 @@
+import faiss
+import json
+import numpy as np
+import pandas as pd
+import pickle
+import torch
+import torch.nn as nn
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi import File
+from fastapi import Form
+from fastapi import UploadFile
+from fastapi.responses import JSONResponse
+from image_processor import process_image_as_pil
+from PIL import Image
+from pydantic import BaseModel
+from torchvision import models
+
+class FeatureExtractor(nn.Module):
+    """
+    A Class for a feature extraction model. Has the same architecture as resnet50.
+    That means it takes in an image in tensor form and returns 1000 outouts.
+    """
+    def __init__(self, decoder: dict = None):
+        super(FeatureExtractor, self).__init__()
+        
+        # Load up resnet50
+        self.resnet50 = models.resnet50(weights='IMAGENET1K_V1')
+        
+        # Set the architecture of the model to be the same as that of resnet50
+        self.layers = torch.nn.Sequential(self.resnet50)
+
+        self.decoder = decoder
+
+    def forward(self, image):
+        """
+        The forward pass of the model
+        """
+        X = image.float()
+        X = self.layers(X)
+        return X
+
+    def predict(self, image):
+        """
+        Same as the forward method, but doesn't keep track of grad to improve compute time
+        """
+        with torch.no_grad():
+            x = self.layers(image)
+            return x
+
+# Don't change this, it will be useful for one of the methods in the API
+class TextItem(BaseModel):
+    text: str
+
+
+
+try:
+    # Set the correct device 
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    # Load the decoder pickle
+    with open("decoder_pickle", 'rb') as decoder_pickle:
+        decoder = pickle.load(decoder_pickle)
+    
+    # Create the model
+    feature_extraction_model = FeatureExtractor(decoder)
+
+    # Load the model to device
+    feature_extraction_model.to(device)
+
+    # Load the parameters
+    model_parameters = torch.load('image_model.pt', map_location=torch.device(device))
+    feature_extraction_model.load_state_dict(model_parameters)
+    print('Model parameters succesfully loaded!')
+    pass
+except:
+    raise OSError("No Feature Extraction model found. Check that you have the decoder and the model in the correct location.")
+
+try:
+    # Load up the json file containing key value pairs of image ids and image embeddings, and store it as 'new_dict'
+    with open('image_embeddings.json', 'r') as file:
+        new_dict = json.load(file)
+
+    # 'Unsqueeze' one layer of the values in new_dict
+    for key, value in new_dict.items():
+        new_dict[key] = [value]
+    
+    # Create a dataframe of the image ids and image embeddings
+    vectors_df = pd.DataFrame.from_dict(new_dict, orient='index', columns=['image embedding'])
+
+    # Create a numpy array with all the image embeddings
+    vectors = np.vstack(vectors_df['image embedding']).astype(np.float32)
+
+    # Initialise the FAISS model
+    index = faiss.IndexFlatL2(1000)
+
+    # Add the image embeddings to the model
+    index.add(vectors)
+    pass
+except:
+    raise OSError("No Image model found. Check that you have the encoder and the model in the correct location.")
+
+
+app = FastAPI()
+print("Starting server")
+
+@app.get('/healthcheck')
+def healthcheck():
+    msg = "API is up and running!"
+    return {"message": msg}
+
+  
+@app.post('/predict/feature_embedding')
+def predict_image(image: UploadFile = File(...)):
+
+    pil_image = Image.open(image.file)
+
+    # Returns a toch tensors from the PIL Image
+    image_tensor = process_image_as_pil(pil_image)
+
+    # Stores the tensor in the device
+    image_tensor = image_tensor.to(device)
+
+    # Extract the image_embeddings from the feature extraction model
+    feature_embeddings = feature_extraction_model(image_tensor)
+
+    # Stores feature embedding tensor as cpu and removes grad attribute so we can cast it as a numpy array
+    feature_embeddings = feature_embeddings.to('cpu')
+    feature_embeddings = feature_embeddings.detach().numpy()
+
+    # Finally cast feature embeddings as a numpy array
+    feature_embeddings = np.array(feature_embeddings)
+
+    feature_embeddings = feature_embeddings.tolist()
+
+    return JSONResponse(content={
+    "features": feature_embeddings, # Return the image embeddings here
+    })
+  
+@app.post('/predict/similar_images')
+def predict_combined(image: UploadFile = File(...), text: str = Form(...)):
+    print(text)
+
+    response = predict_image(image)
+
+    response = response.body
+
+    response = json.loads(response)
+
+    embeddings = response['features']
+
+    embeddings = np.array(embeddings)
+
+    embeddings = embeddings.astype('float32')
+
+    # Use the FAISS algorithm to retrieve the indicies of the closest images
+    nearest_vectors = index.search(embeddings, 3)[1]
+
+    nearest_vectors = nearest_vectors.tolist()
+
+    return JSONResponse(content={
+    "similar_index": nearest_vectors, # Return the index of similar images here
+    })
+
+if __name__ == '__main__':
+    uvicorn.run('api:app', host="0.0.0.0", port=8080)
--- app/image_processor.py
+++ app/image_processor.py
@@ -5,12 +5,6 @@ from clean_images import resize_image
 from PIL import Image
 from torchvision import transforms
 
-def process_image(image_path:str, image_size:int = 224):
-    with Image.open(image_path) as img:
-        cleaned_img = resize_image(image_size, img)
-    image_transformer = transforms.PILToTensor()
-    return image_transformer(cleaned_img).unsqueeze(0)
-
 def process_image_as_pil(image: PIL.Image, image_size:int = 224):
     cleaned_img = resize_image(image_size, image)
     image_transformer = transforms.PILToTensor()
